a62203fc55708fc6f7f8688afa69f56785eae8d1
siddhi1229 <siddhiagarwal1229@gmail.com>
2026-05-17 20:46:58 +0530

    separated routes into modular structure

--- backened/app/main.py
+++ backened/app/main.py
@@ -1,52 +1,26 @@
 from fastapi import FastAPI
 
+from app.routes import cases,suspects
+
 app = FastAPI(
     title="CCIF API",
-    description="Cognitive Criminal Intelligence Fabric",
-    version="1.0"
+    description="Cognitive Criminal Intelligence Fabric"
 )
-
-cases = [
-    {
-        "id":"C-2401",
-        "title":"Harbor Container Theft",
-        "location":"Chennai Port",
-        "status":"Active"
-    },
-    {
-        "id":"C-2402",
-        "title":"Velachery Card Skimming Ring",
-        "location":"Velachery",
-        "status":"Review"
-    }
-]
-
-suspects = [
-    {
-        "id":"S-1001",
-        "name":"Arjun Varma",
-        "risk":92,
-        "gang":"North Quay Syndicate"
-    },
-    {
-        "id":"S-1002",
-        "name":"Meera Khan",
-        "risk":78,
-        "gang":"None"
-    }
-]
-
 
 @app.get("/")
 def home():
-    return {"message":"CCIF Backend Running"}
+    return {
+        "message":"CCIF Backend Running"
+    }
 
+app.include_router(
+    cases.router,
+    prefix="/cases",
+    tags=["Cases"]
+)
 
-@app.get("/cases")
-def get_cases():
-    return cases
-
-
-@app.get("/suspects")
-def get_suspects():
-    return suspects
+app.include_router(
+    suspects.router,
+    prefix="/suspects",
+    tags=["Suspects"]
+)
